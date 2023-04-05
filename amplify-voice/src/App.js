110c340533909b2b293017c0ab9f3d59be47501a
aladevlearning <80547861+aladevlearning@users.noreply.github.com>
2023-04-05 12:38:16 +0200

    Update App.js

--- amplify-voice/src/App.js
+++ amplify-voice/src/App.js
@@ -165,4 +165,4 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default withAuthenticator(App, { hideSignUp: true });
+export default withAuthenticator(App, { hideSignUp: false });
